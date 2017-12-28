03566237827fe441a5d2c22900f8411d27b88f24
Jyothi <33180789+jyothihv2017@users.noreply.github.com>
2017-12-28 07:26:24 -0500

    Add generateCardValues function.

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -9,9 +9,33 @@ var MatchGame = {};
   Generates and returns an array of matching card values.
  */
 
-MatchGame.generateCardValues = function () {
+MatchGame.generateCardValues = function (x,y) {
+
+var generatedNumbers =[];
+var i,z;
+var numGenerated[];
+var inactiveNum[];
+ i=Math.floor(((Math.random() * 10) + 1)-2);
+
+    while(i>= 0 && i<=8) {
+       i=Math.floor(((Math.random() * 10) + 1)-2);
+       generatedNumbers.push(i);
+    while(generatedNumbers.length <= 8) &&  (generatedNumbers.indexOf(i) = 1)){
+    generatedNumbers.push(i);
+  }
+
+  }
+  z = Math.floor(((Math.random() * 10) + 1)-2);
+   numGenerated.push(z);
+   numGenerated.push(z);
+   inactiveNum.push(z);
+     while(inactiveNum.indexOf(z) = 1){
+       return numGenerated[z,z];
+     }
 
 };
+
+
 
 /*
   Converts card values to jQuery card objects and adds them to the supplied game
